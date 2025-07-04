c6565ee59514423799e8ccc791cda3464943fa00
VIISHU-TRICKER-1204 <vishuthakor454@gmail.com>
2025-07-05 01:14:58 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 👿𒁍͟͞»𝑲̭̭̭̭̭𝒊𝒏͡𝒈𝒐̽𝒇𝑻𝒓𝒊͜𝒄̌̌̌̌̌𝒌𝒆͡𝒓̭̭̭̭̭😘🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
